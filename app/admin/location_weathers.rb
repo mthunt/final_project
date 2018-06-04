5de2ce82fc4d0323e817a4a045bca43847e82dc4
Raghu Betina <raghu@firstdraft.com>
2018-06-04 00:30:10 +0000

    Whitelist columns in LocationWeather for ActiveAdmin

--- app/admin/location_weathers.rb
+++ app/admin/location_weathers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register LocationWeather do
+
+ permit_params :location_id, :month, :temperature, :weather
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
